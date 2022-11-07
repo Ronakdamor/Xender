bd64f4178f418c6e92f3a7c4f25c1bfc55f7f863
Ronak Damor <97729986+Ronakdamor@users.noreply.github.com>
2022-11-07 21:13:17 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+pkg install figlet
+clear
+figlet -f banner "setup for xender"
+sleep 2
+pkg install figlet
+pkg install python
+pkg install python2
+pkg install python3
+ln -s /storage/emulated/0/
+
+echo "\033[0;32m setup ready . now you run this command ::   ./xender.sh "
